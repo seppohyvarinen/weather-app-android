9ff429ffa8643ed4fc397d976216ce11e5ce4ef4
Seppo Hyvärinen <seppo.hyvarinen@.tuni.fi>
2022-05-10 10:31:47 +0300

    Add: weather description parsed and shows on UI

--- app/src/main/java/fi/tuni/weather_app_kotlin/WeatherDescriptionObject.kt
+++ app/src/main/java/fi/tuni/weather_app_kotlin/WeatherDescriptionObject.kt
@@ -1,8 +1,5 @@
 package fi.tuni.weather_app_kotlin
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class WeatherMain(var temp: Double? = 0.0, var feels_like : Double? = 0.0) {
-
-}
+data class WeatherDescriptionObject(var description : String? = null)
