249b87c561c096473a532cf7d01ca6bcb877971d
Seppo Hyvärinen <seppo.hyvarinen@.tuni.fi>
2022-05-16 12:46:11 +0300

    Add: fetching the forecast now in forecast activity, result logs OK

--- app/src/main/java/fi/tuni/weather_app_kotlin/ForecastActivity.kt
+++ app/src/main/java/fi/tuni/weather_app_kotlin/ForecastActivity.kt
@@ -1,29 +1,94 @@
 package fi.tuni.weather_app_kotlin
 
+import android.app.Activity
 import android.os.Bundle
 import android.util.Log
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.google.android.gms.location.LocationServices
+import fi.tuni.weather_app_kotlin.MainActivity
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
+import kotlin.math.roundToInt
 
-class ForecastActivity : AppCompatActivity(){
+class ForecastActivity : AppCompatActivity() {
 
-    lateinit var extraBox : TextView
+    lateinit var extraBox: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forecast)
         extraBox = findViewById<TextView>(R.id.extraTest)
-        var extras : Bundle? = intent.extras
+        var extras: Bundle? = intent.extras
 
 
 
         if (extras != null) {
-            val extraTest : String? = extras.getString("latitude")
-            if (extraTest != null) {
-                extraBox.text = extraTest
+            val lat: String? = extras.getString("latitude")
+            val lon: String? = extras.getString("longitude")
+            if (lat != null && lon != null) {
+                downloadUrlAsync(
+                    this,
+                    "https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&cnt=7&appid=bc2d40bf4e1d09c80f0383a56d873af0"
+                ) {
+                    if (it != null) {
+
+                        Log.d("test", it)
+                        val mp = ObjectMapper()
+                        val myObject: WeatherJsonObject = mp.readValue(
+                            it,
+                            WeatherJsonObject::class.java
+                        )
+
+
+                    }
+
+
+                }
+            }
+
+
+        }
+
+    }
+
+    private fun downloadUrlAsync(a: Activity, s: String, function: (l: String?) -> Unit) {
+        thread() {
+            val lines: String? = getUrl(s)
+
+            a.runOnUiThread() {
+                function(lines)
             }
         }
+
+    }
+
+    fun getUrl(url: String): String? {
+
+        val myUrl = URL(url)
+        val conn = myUrl.openConnection() as HttpURLConnection
+
+        if (conn.responseCode != 404) {
+            val reader = BufferedReader(InputStreamReader(conn.getInputStream()));
+
+            return buildString {
+                reader.use {
+                    var line: String? = null
+                    do {
+                        line = it.readLine()
+                        append(line)
+                    } while (line != null)
+                }
+            }
+        } else {
+
+            return null
+        }
+
 
     }
 }
